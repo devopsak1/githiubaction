e4d2d162dc85465d783411364b8204adca84c0a7
devopsak1 <devopssystemserver@gmail.com>
2024-08-23 23:57:49 +0530

    file added.....

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+data = {
+    'Course Name': ['Devops', 'Azure', 'Snowflake', 'SQL', 'Hadoop'],
+    'Course Duration': ['30 Day', '35 Day', '40 Day', '30 Day', '21 Day']
+}
+
+df = pd.DataFrame(data)
+st.write('Course Details Table....')
